82da7af4ef36e288fabe2c334d199d7cffe91c00
matveikulakovskiii <matveikulakovski@gmail.com>
2022-12-14 11:35:46 +0200

    if, eli, try, except kasutamine tingimuslik operaator

--- Funktsioonide_print___input____float____int___kasutamine.py
+++ Funktsioonide_print___input____float____int___kasutamine.py
@@ -8,10 +8,15 @@
 
 
 #print("Puu läbimõõdu arvutamine")
-#С=float(input("Määrake puu ümbermõõt -> "))
-#d=С/pi
-#print("Puu läbimõõt on", round(d,2))
-#print()
+#try:
+#    С=float(input("Määrake puu ümbermõõt -> "))
+#    if С>0:
+#        d=round(С/pi,2)
+#        print(f"Puu läbimõõt on {d}")
+#    else:
+#        print("C peab olema suurem kui 0")
+#except:
+#    print("Andmetüüd on vale")
 
 
 #2.
@@ -21,33 +26,49 @@
 #import math
 
 #print("ristküliku omadused")
-#M=float(input("ristküliku esimene külg -> "))
-#N=float(input("ristküliku teine külg -> "))
-#c=math.sqrt(N**2+M**2)
-#print("ristküliku pikkus on", round(c,2))
-#print()
+#try:
+#    M=float(input("ristküliku esimene külg -> "))
+#    N=float(input("ristküliku teine külg -> "))
+#    if M>0 and N>0:
+#        d=math.sqrt(N**2+M**2)
+#        print("ristküliku pikkus on", round(d,2))
+#    else:
+#        print("M ja N peab olema suurem kui 0")
+#except: 
+#    print("Andmetüüd on vale")
 
 #3.
 #Найдите семантическую ошибку в следующем примере программы:
 
-#aeg = float(input("Mitu tundi kulus sõiduks? "))
-#teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
-#kiirus = teepikkus / aeg
-
-#print("Sinu kiirus oli " + str(kiirus) + " km/h")
-#print()
+#try: 
+#    aeg = float(input("Mitu tundi kulus sõiduks? "))
+#    teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+#    if aeg>0 and teepikkus>0:
+#        kiirus = teepikkus / aeg
+#        print("Sinu kiirus oli " + str(kiirus) + " km/h")
+#    else:
+#        print("aeg ja teepikkus peab olema suurem kui 0")
+#except:
+#    print("Andmetüüd on vale")
 
 #4.
 #Koostada programm, mis arvutab aritmeetilise keskmise suvalisest etteantud 5 täis arvustprint
 
-#print("määrake mis tahes viis täisarvu")
-#a=float(input("Sisestage mis tahes täisarv -> "))
-#b=float(input("Sisestage mis tahes täisarv -> "))
-#c=float(input("Sisestage mis tahes täisarv -> "))
-#d=float(input("Sisestage mis tahes täisarv -> "))
-#e=float(input("Sisestage mis tahes täisarv -> "))
-#h=(a+b+c+d+e)/5
-#print("aritmeetiline keskmine on", h)
+#try:
+#    print("määrake mis tahes viis täisarvu")
+#    a=float(input("Sisestage mis tahes täisarv -> "))
+#    b=float(input("Sisestage mis tahes täisarv -> "))
+#    c=float(input("Sisestage mis tahes täisarv -> "))
+#    d=float(input("Sisestage mis tahes täisarv -> "))
+#    e=float(input("Sisestage mis tahes täisarv -> "))
+#    if a>0 and b>0 and c>0 and d>0 and e>0:
+#        h=(a+b+c+d+e)/5
+#        print("aritmeetiline keskmine on", h)
+#    else:
+#        print("a, b, c, d, e peab olema suurem kui 0")
+#except:
+#    print("Andmetüüd on vale")
+
 
 #5.
 #Joonista samasugune konn
@@ -62,24 +83,34 @@
 
 #from random import *
 
-#a=randint(0,100)
-#b=randint(0,100)
-#c=randint(0,100)
-#print(f"a={a}\n,b={b}\n,c={c}")
-#P=a+b+c
-#print(f"Ümbermõõt on {P}")
+#try:
+#    a=randint(0,100)
+#    b=randint(0,100)
+#    c=randint(0,100)
+#    if a>0 and b>0 and c>0:
+#        print(f"a={a}\n,b={b}\n,c={c}")
+#        P=a+b+c
+#        print(f"Ümbermõõt on {P}")
+#    else:
+#        print("a, b, c peab olema suurem kui 0")
+#except:
+#    print("Andmetüüd on vale")
 
 
 #7.
 #Pitsa
-#    Võtsite P sõbraga suure pitsa hinnaga 12,90€
-#    Jätate teenindajale 10% jootraha
-#    Koosta programm, mis leiab kui palju peab igaüks maksma
+    #Võtsite P sõbraga suure pitsa hinnaga 12,90€
+    #Jätate teenindajale 10% jootraha
+    #Koosta programm, mis leiab kui palju peab igaüks maksma
 
-#P=randint(1,6)
-#summa=(12.9*1.1)/P 
-#print(f"{P}-st, Igaüks maksab {summa} ")
+    #from random import *
 
+#try: 
+#    P=randint(1,6)
+#    summa=(12.9*1.1)/P 
+#    print(f"{P}-st, Igaüks maksab {summa} ")
+#except:
+#    print("Andmetüüd on vale")
 
 #8.
 #Kütusekulu arvutamine
@@ -88,11 +119,17 @@
 #    Kasutaja sisestab läbitud kilomeetrid
 #    Programm leiab kütusekulu 100km kohta keskmiselt
 
-#print("kütsusekulu arvutamine")
-#l=float(input("Kütuse liitride kogus -> "))
-#km=float(input("Läbitud kilomeetrid -> "))
-#kulu=l/km*100
-#print(f"kütusekulu arvutamine 100 kilomeetri kohta on võrdne {kulu}")
+#try:
+#    print("kütsusekulu arvutamine")
+#    l=float(input("Kütuse liitride kogus -> "))
+#    km=float(input("Läbitud kilomeetrid -> "))
+#    if l>0 and km>0:
+#        kulu=l/km*100
+#        print(f"kütusekulu arvutamine 100 kilomeetri kohta on võrdne {kulu}")
+#    else:
+#        print("l ja km peab olema suurem kui 0")
+#except:
+#    print("Andmetüüd on vale")
 
 
 
@@ -102,11 +139,17 @@
 #    Rulluisutaja keskmine kiirus on 29,9km/h
 #    Kui kaugele jõuab M minutiga
 
-#print("Rulluisutajad")
-#M=int(input("Minutid -> "))
-#M=M/60
-#tee=M*29.9
-#print(f"Jõuab {tee} km")
+#try:
+#    print("Rulluisutajad")
+#    M=int(input("Minutid -> "))
+#    if M>0:
+#        M=M/60
+#        tee=M*29.9
+#        print(f"Jõuab {tee} km")
+#    else:
+#        print("M peab olema suurem kui 0")
+#except:
+#    print("Andmetüüd on vale")
 
 
 #10.
@@ -116,8 +159,35 @@
 #    Sinu valem leiab ja väljastab tunnid ja minutid
 #    Näiteks: sisestus 72, vastus 1:12
 
-#print("Ajateisendus")
-#M=int(input("Sisesta aja minutites -> "))
-#H=M//60 #h
-#M=M%60 #min
-#print(f"{H}:{M}")
+#try:
+#    print("Ajateisendus")
+#    M=int(input("Sisesta aja minutites -> "))
+#    if M>0:
+#        H=M//60 #h
+#        M=M%60 #min
+#        print(f"{H}:{M}")
+#    else:
+#        print("M peab olema suurem kui 0")
+#except:
+#    print("Andmetüüd on vale")
+
+
+
+
+
+#print("Ema roobot")
+#a=input("Sisesta: ")
+#print(a.isalpha(), a.isdigit())
+#if a.isdigit() and int(a)>0 and int(a)==5:
+#    a=int(a)
+#    if a==5:
+#        print("Hea")
+#    elif a==4:
+#        print("Hästi")
+#    elif a==3:
+#        print("Keskmine")
+#    elif a==2 and a==1:
+#        print("Halvasti")
+#else:
+#    print("Sa valesti vastas")
+
